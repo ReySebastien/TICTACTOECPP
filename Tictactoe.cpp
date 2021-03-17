e658df9c33348dcd89f78a6ec675cbb09b387dc4
ReySebastien <rey.babou35@free.fr>
2021-03-17 10:49:54 +0100

    update

--- Tictactoe.cpp
+++ Tictactoe.cpp
@@ -9,33 +9,46 @@
 }
 
     void Tictactoe::afficheGrille(){
-        for(int j=0;j<3;j++){
-            for(int i=0;i<3;i++){
-                std::cout << this-> ajouteSymbole(i,j);
-                if(i!=3)  std::cout << " | ";
-            }
-            std::cout << std::endl;
-            if(j!=3)  std::cout << " ------- " << std::endl;
-        }
+       grilleDeJeu.affiche();
+       std::cout << std::endl;
     }
 
     void Tictactoe::ajouteSymbole(int x, int y){
+        grilleDeJeu.setContent(x, y, symboleCourant);
 
     }
 
     bool Tictactoe::testeVictoireVerticale(){
-
+        for(int i=0, i<3, i++){
+            if(grilleDeJeu.getContent(i,0) != ' ' && grilleDeJeu.getContent(i,0) == grilleDeJeu.getContent(i,1) && grilleDeJeu.getContent(i,0) == grilleDeJeu.getContent(i,2)){
+                std::cout << "Gagné" << std::endl; 
+                return true;
+            }
+        }
     }
 
     bool Tictactoe::testeVictoireHorizontale(){
-
+        for(int i=0, i<3, i++){
+            if(grilleDeJeu.getContent(0,i) != ' ' && grilleDeJeu.getContent(0,i) == grilleDeJeu.getContent(1,i ) && grilleDeJeu.getContent(0,i) == grilleDeJeu.getContent(2,i)){
+                std::cout << "Gagné" << std::endl; 
+                return true;
+            }
+        }
     }
 
     bool Tictactoe::testeVictoireDiagonale(){
-
+        for(int i=0, i<3, i++){
+            if(grilleDeJeu.getContent(i,i) != ' ' && grilleDeJeu.getContent(i,i) == grilleDeJeu.getContent(i+1,i+1) && grilleDeJeu.getContent(i, i) == grilleDeJeu.getContent(i+2,i+2)){
+                std::cout << "Gagné" << std::endl; 
+                return true;
+            }
+        }
     }
 
     bool Tictactoe::testeJeuNul(){
+        if(numeroTour == 9){
+            return true;
+        }
 
     }
 
