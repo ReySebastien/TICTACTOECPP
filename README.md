914c5cbd212d5dcc021a0537725fd4574931ee0c
ReySebastien <rey.babou35@free.fr>
2021-03-17 09:07:22 +0100

    Initialisation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# TICTACTOECPP
+# Tictactoe
+ 
---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "Grille.cpp"
+#include "Tictactoe.cpp"
+#include <iostream>
+#include <string>
+
+
+int main(){
+
+    bool partiegagnee = false;
+    Tictactoe jeu = Tictactoe();
+    int numeroCase;
+
+
+    while(!partiegagnee || jeu.testeJeuNul()){
+        
+        jeu.afficheGrille();
+        std::cin >> numeroCase;
+        jeu.ajouteSymbole(numeroCase%3,numeroCase/3);
+        partiegagnee = jeu.testeVictoireDiagonale() && jeu.testeVictoireHorizontale() && jeu.testeVictoireVerticale();
+        jeu.finTour();
+    }
+
+    
+    
+    return 0;
+}
